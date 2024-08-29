9bb472f98555ef2b96fb6efd7f7652cf5cb72363
Chuicks <eyzhang@ucsd.edu>
2024-08-29 15:57:23 -0400

    TIcTacToes started

---
+++ src/TicTacToe.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+    int boardWidth = 600;
+    int boardHeight = 650; 
+
+    JFrame frame = new JFrame("Tic Tac Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    
+
+    TicTacToe(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.gray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Verdana", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic Tac Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        //boardPanel.setLayout(new Grid)
+    }
+
+}

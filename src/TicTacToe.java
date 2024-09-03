c7bd42caec724eb52172f4d7ad2234fdbba30eb3
Chuicks <eyzhang@ucsd.edu>
2024-09-03 16:01:48 -0400

    Begun singleplayer work, finished instablock

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.*;
 
 public class TicTacToe {
@@ -36,8 +38,81 @@ public class TicTacToe {
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());
-        }
+
+            //Label
+            textLabel.setBackground(Color.gray);
+            textLabel.setForeground(Color.white);
+            textLabel.setFont(new Font("Verdana", Font.BOLD, 50));
+            textLabel.setHorizontalAlignment(JLabel.CENTER);
+            textLabel.setText("  Tic Tac Toe");
+            textLabel.setOpaque(true);
+            textPanel.setBackground(Color.gray);
+            textPanel.setLayout(new BorderLayout());
+            textPanel.add(textLabel, BorderLayout.WEST);
+            
+            //retry button
+            JButton retryButton = new JButton();  
+            retryButton.setBackground(Color.gray);
+            retryButton.setForeground(Color.white);
+            retryButton.setFont(new Font("Verdana", Font.BOLD, 17));
+            retryButton.setText("Reset");
+            retryButton.setFocusable(false);
+            textPanel.add(retryButton, BorderLayout.EAST);
+            frame.add(textPanel, BorderLayout.NORTH);
+ 
+            retryButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    textLabel.setText("  Tic Tac Toe");
+                    for(int r = 0; r < 3; r ++){
+                        for(int c = 0; c < 3; c ++){
+                            board[r][c].setForeground(Color.white);
+                            board[r][c].setText("");
+                        }
+                    }
+                    currentPlayer = player_X;
+                    gameOver = false;
+                    turns = 0;
+                }
+            });
+            
+              
+
+            //Playing Grid
+            boardPanel.setLayout(new GridLayout(3,3));
+            boardPanel.setBackground(Color.darkGray);
+            currentPlayer = player_X;
+            frame.add(boardPanel);
         
+            for(int r = 0; r < 3; r++){
+                for(int c = 0; c < 3; c++){
+                    JButton tile = new JButton();
+                    board[r][c] = tile;
+                    boardPanel.add(tile);
+
+                    tile.setBackground(Color.darkGray);
+                    tile.setForeground(Color.white);
+                    tile.setFont(new Font("Verdana", Font.BOLD, 120));
+                    tile.setFocusable(false);
+
+                    tile.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e){
+                            if(gameOver) return;
+                            JButton tile = (JButton) e.getSource();
+                            if(tile.getText() == ""){
+                                tile.setText(currentPlayer);
+                                turns ++;
+                                checkWinner();
+                                autoBot();
+                                if(!gameOver){
+                                    textLabel.setText("  " + currentPlayer + "'s turn.");
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+        /* 
         //Two player mode
         if(mode == 2){
             //Frame
@@ -119,6 +194,7 @@ public class TicTacToe {
                 }
             }
         }
+        */
     }
 
     void setWinner(JButton tile){
@@ -138,7 +214,6 @@ public class TicTacToe {
                 return;
             }
         }
-
         //vertical
         for(int c = 0; c < 3; c ++){
             if(board[0][c].getText() == "") continue;
@@ -150,7 +225,6 @@ public class TicTacToe {
                 return;
             }
         }
-
         //diagonal1
         if (board[0][0].getText() == board[1][1].getText() && 
         board[1][1].getText() == board[2][2].getText() &&
@@ -161,7 +235,6 @@ public class TicTacToe {
             gameOver = true;
             return;
         }
-
         //diagonal2
         if (board[0][2].getText() == board[1][1].getText() && 
         board[1][1].getText() == board[2][0].getText() &&
@@ -172,7 +245,6 @@ public class TicTacToe {
             gameOver = true;
             return;
         }
-
         //tie
         if(turns == 9){
             for(int r = 0; r < 3; r ++){
@@ -184,5 +256,154 @@ public class TicTacToe {
             gameOver = true;
             return;
         }
+    }
+
+
+    //!!have to check win then check block or it will just pick first priority one and pick
+    void autoBot(){
+        //X move 1 (8 possible)
+        //If opponent doesn't have center, we take it. If they have center, we play top left corner
+        if(turns == 1){
+            if(board[1][1].getText() == ""){
+                board[1][1].setText(player_O);
+            }
+            else{
+                board[0][0].setText(player_O); 
+            }
+            turns ++;
+        }  
+
+        //X move 2 (6 possible)
+        if(turns == 3){
+            //we don't have mid
+            if(board[1][1].getText() == player_X){
+                //2c
+                if(board[2][2].getText() == player_X){
+                    board[0][2].setText(player_O);
+                }
+                //2o, we block
+                else{
+                    botCheck();
+                }
+            }
+            //we have mid
+            else{
+                if(!botCheck()){
+                    //pick a corner
+                    int corners = cornerCount();
+                    System.out.println(corners);
+                    //pick any edge
+                    if(corners == 2){
+                        board[0][1].setText(player_O);
+                    }
+                }          
+            }
+            turns ++;
+        }  
+        //X move 3 (3 possible)
+        //can win
+        if(turns == 5){
+            //board[1][1].setText(player_O); 
+            //botWin();
+            turns ++;
+        } 
+        //X move 4 (1 possible)
+        //Scan's for only possible move left and complete
+        if(turns == 7){
+            //board[0][1].setText(player_O); 
+            //botWin();
+            turns ++;
+        } 
+    }
+
+    boolean botCheck(){
+        //horizontal 
+        for(int r = 0; r < 3; r ++){
+            //110
+            if(board[r][0].getText() == board[r][1].getText() && board[r][0].getText() != "" && board[r][2].getText() == ""){
+                board[r][2].setText(player_O);
+                return true;
+            }
+            //011
+            if(board[r][1].getText() == board[r][2].getText() && board[r][1].getText() != "" && board[r][0].getText() == ""){
+                board[r][0].setText(player_O);
+                return true;
+            }
+            //101 
+            if(board[r][0].getText() == board[r][2].getText() && board[r][0].getText() != "" && board[r][1].getText() == ""){
+                board[r][1].setText(player_O);
+                return true;
+            }
+        }
+        //vertical
+        for(int c = 0; c < 3; c ++){
+            //110
+            if(board[0][c].getText() == board[1][c].getText() && board[0][c].getText() != "" && board[2][c].getText() == ""){
+                board[2][c].setText(player_O);
+                return true;
+            }
+            //011
+            if(board[1][c].getText() == board[2][c].getText() && board[1][c].getText() != "" && board[0][c].getText() == ""){
+                board[0][c].setText(player_O);
+                return true;
+            }
+            //101
+            if(board[0][c].getText() == board[2][c].getText() && board[0][c].getText() != "" && board[1][c].getText() == ""){
+                board[1][c].setText(player_O);
+                return true;
+            }
+        }
+        //diagonal1
+        //110
+        if(board[0][0].getText() == board[1][1].getText() && board[0][0].getText() != "" && board[2][2].getText() == ""){
+            board[2][2].setText(player_O);
+            return true;
+        }
+        
+        //011
+        if(board[1][1].getText() == board[2][2].getText() && board[1][1].getText() != "" && board[0][0].getText() == ""){
+            board[0][0].setText(player_O);
+            return true;
+        }
+        //101
+        if(board[0][0].getText() == board[2][2].getText() && board[0][0].getText() != "" && board[1][1].getText() == ""){
+            board[1][1].setText(player_O);
+            return true;
+        }
+        //diagonal2
+        //110
+        if(board[0][2].getText() == board[1][1].getText() && board[0][2].getText() != "" && board[2][0].getText() == ""){
+            board[2][0].setText(player_O);
+            return true;
+        }
+        
+        //011
+        if(board[1][1].getText() == board[2][0].getText() && board[1][1].getText() != "" && board[0][2].getText() == ""){
+            board[0][2].setText(player_O);
+            return true;
+        }
+        //101
+        if(board[0][2].getText() == board[2][0].getText() && board[0][2].getText() != "" && board[1][1].getText() == ""){
+            board[1][1].setText(player_O);
+            return true;
+        }
+        return false;
+    }    
+
+    int cornerCount(){
+        int count = 0;
+        if(board[0][0].getText() != ""){
+            count ++;
+        }
+        if(board[0][2].getText() != ""){
+            count ++;
+        }
+        if(board[2][0].getText() != ""){
+            count ++;
+        }
+        if(board[2][2].getText() != ""){
+            count ++;
+        }
+        return count;
     }
 }
